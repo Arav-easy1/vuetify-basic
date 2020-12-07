c67b3cb50c457fc0ebcd1578c9fe5abf0804b8fe
leejiwon <wg0418@gmail.com>
2020-12-07 06:47:12 +0000

    basic created App with Vuetify

---
+++ src/views/About.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="about pa-6">
+    <h1>About veutify Todo</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi cupiditate sunt odit laboriosam, debitis error adipisci a hic consectetur sint, mollitia tempore fugit esse quaerat? Enim sapiente fugit eum corrupti.</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi cupiditate sunt odit laboriosam, debitis error adipisci a hic consectetur sint, mollitia tempore fugit esse quaerat? Enim sapiente fugit eum corrupti.</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi cupiditate sunt odit laboriosam, debitis error adipisci a hic consectetur sint, mollitia tempore fugit esse quaerat? Enim sapiente fugit eum corrupti.</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi cupiditate sunt odit laboriosam, debitis error adipisci a hic consectetur sint, mollitia tempore fugit esse quaerat? Enim sapiente fugit eum corrupti.</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi cupiditate sunt odit laboriosam, debitis error adipisci a hic consectetur sint, mollitia tempore fugit esse quaerat? Enim sapiente fugit eum corrupti.</p>
+  </div>
+</template>
